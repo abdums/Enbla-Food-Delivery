d2758e3cf0b2cacefd49b69f3f51ffa79286d87f
ZEAB-H <abelhailemichael30@gmail.com>
2023-08-03 09:41:44 +0300

    project file updated

--- schemas/index.js
+++ schemas/index.js
@@ -1,6 +1,9 @@
 import blockContent from './blockContent'
 import category from './category'
 import post from './post'
+import dish from './dish'
 import author from './author'
+import featured from './featured'
+import restaurant from './restaurant'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [category, dish,restaurant,featured]
